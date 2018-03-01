83b247fda133d4c0b7e424dc334ab684e6327ddb
Brett Sullivan <opticalfalicy@gmail.com>
2018-02-28 18:41:43 -0600

    Travelers Finished - Extra Credit Started

--- index.js
+++ index.js
@@ -5,47 +5,74 @@ const greenBlock = document.getElementsByClassName("block--green")[0];
 const pinkBlock = document.getElementsByClassName("block--pink")[0];
 const grayBlock = document.getElementsByClassName("block--gray")[0];
 const blockParent = document.getElementsByClassName("blocks")[0];
-redBlock.addEventListener("click", (event) => {
-    redBlock.style.order = -1;
-    blueBlock.style.order = 0;
-    greenBlock.style.order = 0;
-    pinkBlock.style.order = 0;
-    grayBlock.style.order = 0;
-    
-    
+document.getElementsByClassName("blocks")[0].style.width = "100%";
+
+
+blockParent.style.width = "100%"; 
+
+blockParent.addEventListener("mousedown", (event) =>{
+    function slide(blockX){
+       
+            blockX.style.alignSelf = "center";
+
+            setTimeout(function() {
+                blockX.style.alignSelf = "flex-end";
+            }, 1000)
+
+            
+    }
+
+    redBlock.addEventListener("mousedown", (event) => {
+        slide(redBlock);
+    })
+
+    blueBlock.addEventListener("mousedown", (event) => {
+        slide(blueBlock);
+    })
+
+    greenBlock.addEventListener("mousedown", (event) => {
+        slide(greenBlock);
+    })
+    pinkBlock.addEventListener("mousedown", (event) => {
+        slide(pinkBlock);
+    })
+    grayBlock.addEventListener("mousedown", (event) => {
+        slide(grayBlock);
+    })
+
+} )
+
+blockParent.addEventListener("mouseup", (event) => {
+    function unSlide(blockY){
+        blockY.style.alignSelf = "center";
+
+        // setTimeout(function() {
+        //     blockX.style.alignSelf = "flex-start"
+        // }, 5000)
+    }
+
+    redBlock.addEventListener("mouseup", (event) => {
+        unSlide(redBlock);
+    })
+
+    blueBlock.addEventListener("mouseup", (event) => {
+        unSlide(blueBlock);
+    })
+
+    greenBlock.addEventListener("mouseup", (event) => {
+        unSlide(greenBlock);
+    })
+    pinkBlock.addEventListener("mouseup", (event) => {
+        unSlide(pinkBlock);
+    })
+    grayBlock.addEventListener("mouseup", (event) => {
+        unSlide(grayBlock);
+    })
 })
-blueBlock.addEventListener("click", (event) => {
-    redBlock.style.order = 0;
-    blueBlock.style.order = -1;
-    greenBlock.style.order = 0;
-    pinkBlock.style.order = 0;
-    grayBlock.style.order = 0;
-    
-    
-})
-greenBlock.addEventListener("click", (event) => {
-    redBlock.style.order = 0;
-    blueBlock.style.order = 0;
-    greenBlock.style.order = -1;
-    pinkBlock.style.order = 0;
-    grayBlock.style.order = 0;
-    
-    
-})
-pinkBlock.addEventListener("click", (event) => {
-    redBlock.style.order = 0;
-    blueBlock.style.order = 0;
-    greenBlock.style.order = 0;
-    pinkBlock.style.order = -1;
-    grayBlock.style.order = 0;
-    
-    
-})
-grayBlock.addEventListener("click", (event) => {
-    redBlock.style.order = 0;
-    blueBlock.style.order = 0;
-    greenBlock.style.order = 0;
-    pinkBlock.style.order = 0;
-    grayBlock.style.order = -1;
-    
- })
+
+
+// slide(redBlock);
+// slide(blueBlock);
+// redBlock.addEventListener("mousedown", (event) =>{
+//     redBlock.style.alignSelf = "center";
+// })
